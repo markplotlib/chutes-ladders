49e52088ba5d37af82b9e65c64cd83f87fb72d0e
Mark Chesney <mark.chesney@gmail.com>
2019-07-14 21:30:20 -0700

    end game immediately when a player reaches 100

--- Chutes.java
+++ Chutes.java
@@ -1,47 +1,51 @@
 public class Chutes {
 
-			// constants of the game's board
-			public final int NUM_SQUARES = 100;
+	// constants of the game's board
+	public final int NUM_SQUARES = 100;
 
-			public static void main(String[] args) {
-				// variable declaration
 
-				// String variables
-				String welcome = "Welcome to Chutes & Ladders!  ...  ";
 
-				int numPlayers = 2;  // (btw final code is done)
+	public static void main(String[] args) {
+		// variable declaration
 
-				// With numPlayers assigned by user, array lengths can be fixed.
-				String[] playerNames = {"Abe", "Bob"};  // names of players
+		// String variables
+		String welcome = "Welcome to Chutes & Ladders!  ...  ";
 
-				int[] playerPositions = new int[numPlayers];	// positions of players
+		int numPlayers = 2;  // (btw final code is done)
 
-				// declarations of inner-loop variables
-				int playerNum = 0;
-				int spinner;
+		// With numPlayers assigned by user, array lengths can be fixed.
+		String[] playerNames = {"Abe", "Bob"};  // names of players
 
-				while (playerPositions[playerNum] != 100) {
+		int[] playerPositions = new int[numPlayers];	// positions of players
 
-					while (playerNum < numPlayers && playerPositions[playerNum] != 100) {
-			         System.out.printf("%s, it's your turn. You are currently at space %d.\n", playerNames[playerNum], playerPositions[playerNum]);
+		// declarations of inner-loop variables
+		int playerNum = 0;
+		int spinner;
 
-			         spinner = 50;
-			         System.out.printf("The spin was: %d\n", spinner);
-						if (playerPositions[playerNum] > 100) {
-						   System.out.printf("Sorry, no player can go over 100.\n\n");
-						} else {
-						   playerPositions[playerNum] += spinner;
-							System.out.printf("You are now at space %d.\n\n", playerPositions[playerNum]);
-						}
+		while (playerPositions[playerNum] != 100)
+		{
+			System.out.printf("Player %d.\n", playerNum+1);
+         System.out.printf("%s, it's your turn. You are currently at space %d.\n", playerNames[playerNum], playerPositions[playerNum]);
 
-			      	playerNum++;
-					}
-
-					playerNum = 0;
-
-				}
-				System.out.printf("%s, you have won the game!\n\n", playerNames[playerNum]);
-
+         spinner = 50;
+         System.out.printf("The spin was: %d\n", spinner);
+			if (playerPositions[playerNum] + spinner > 100) {
+			   System.out.printf("Sorry, no player can go over 100.\n\n");
+			} else {
+			   playerPositions[playerNum] += spinner;
+				System.out.printf("You are now at space %d.\n\n", playerPositions[playerNum]);
 			}
-
+			if (playerPositions[playerNum] != 100) {
+				playerNum++;
+				if (playerNum == numPlayers) {
+					playerNum = 0;
+				}
+			}
 		}
+
+		System.out.printf("%s, you have won the game!\n\n", playerNames[playerNum]);
+
+
+	}
+
+}
