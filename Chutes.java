b4d4a70d4c5d834469e7b1469d4405a86d465036
Mark Chesney <mark.chesney@gmail.com>
2019-07-14 23:17:06 -0700

    Ask user to play again after player wins

--- Chutes.java
+++ Chutes.java
@@ -1,4 +1,4 @@
-public class Chutes {
+public class Dev {
 
 	// constants of the game's board
 	public final int NUM_SQUARES = 100;
@@ -7,43 +7,48 @@ public class Chutes {
 
 	public static void main(String[] args) {
 		// variable declaration
+		String responseToRepeat;
+		char repeat;
 
-		// String variables
-		String welcome = "Welcome to Chutes & Ladders!  ...  ";
+		do {
+			int numPlayers = 2;  // (btw final code is done)
 
-		int numPlayers = 2;  // (btw final code is done)
+			// With numPlayers assigned by user, array lengths can be fixed.
+			String[] playerNames = {"Abe", "Bob"};  // names of players
 
-		// With numPlayers assigned by user, array lengths can be fixed.
-		String[] playerNames = {"Abe", "Bob"};  // names of players
+			int[] playerPositions = new int[numPlayers];	// positions of players
 
-		int[] playerPositions = new int[numPlayers];	// positions of players
+			// declarations of inner-loop variables
+			int playerNum = 0;
+			int spinner;
 
-		// declarations of inner-loop variables
-		int playerNum = 0;
-		int spinner;
+			while (playerPositions[playerNum] != 100)
+			{
+	         System.out.printf("%s, it's your turn. You are currently at space %d.\n", playerNames[playerNum], playerPositions[playerNum]);
 
-		while (playerPositions[playerNum] != 100)
-		{
-         System.out.printf("%s, it's your turn. You are currently at space %d.\n", playerNames[playerNum], playerPositions[playerNum]);
-
-         spinner = 50;
-         System.out.printf("The spin was: %d\n", spinner);
-			if (playerPositions[playerNum] + spinner > 100) {
-			   System.out.printf("Sorry, no player can go over 100.\n\n");
-			} else {
-			   playerPositions[playerNum] += spinner;
-				System.out.printf("You are now at space %d.\n\n", playerPositions[playerNum]);
-			}
-			if (playerPositions[playerNum] != 100) {
-				playerNum++;
-				if (playerNum == numPlayers) {
-					playerNum = 0;
+	         spinner = 50;
+	         System.out.printf("The spin was: %d\n", spinner);
+				if (playerPositions[playerNum] + spinner > 100) {
+				   System.out.printf("Sorry, no player can go over 100.\n\n");
+				} else {
+				   playerPositions[playerNum] += spinner;
+					System.out.printf("You are now at space %d.\n\n", playerPositions[playerNum]);
+				}
+				if (playerPositions[playerNum] != 100) {
+					playerNum++;
+					if (playerNum == numPlayers) {
+						playerNum = 0;
+					}
 				}
 			}
-		}
 
-		System.out.printf("%s, you have won the game!\n\n", playerNames[playerNum]);
+			System.out.printf("%s, you have won the game!\n\n", playerNames[playerNum]);
 
+			System.out.print("Do you want to play again (y/n)? ");
+			responseToRepeat = keyboard.nextLine();
+			repeat = responseToRepeat.charAt(0);
+
+		} while (repeat == 'y' || repeat == 'Y');
 
 	}
 
